dcd64a48c9f09c5cb45a96df225ca775f1eb31aa
KartikSharma1k <60931361+KartikSharma1k@users.noreply.github.com>
2024-03-12 01:11:11 +0530

    Fifth Commit - Kartik Sharma (12/03/2024 @ 01:10 A.M)

--- app/src/main/java/com/instagram_clone/screens/SearchScreen.kt
+++ app/src/main/java/com/instagram_clone/screens/SearchScreen.kt
@@ -1,14 +1,22 @@
 package com.instagram_clone.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -28,13 +36,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import com.instagram_clone.DataManager
 import com.instagram_clone.R
 import com.instagram_clone.models.PostData
@@ -142,8 +154,79 @@ fun TopAppSearchBar() {
                 }
         },
         placeholder = { Text(text = "Search") },
-        colors = SearchBarDefaults.colors(containerColor = Color.DarkGray),
+        colors = SearchBarDefaults.colors(
+            containerColor = Color.Black,
+            dividerColor = Color.DarkGray
+        ),
     ) {
 
+        LazyColumn(
+            modifier = Modifier.padding(
+                start = 15.dp
+            )
+        ) {
+            item {
+                Text(
+                    text = "Recent",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(vertical = 15.dp)
+                )
+            }
+            items(count = 10) {
+                SearchHistoryItem()
+            }
+        }
     }
+}
+
+@Composable
+fun SearchHistoryItem() {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(vertical = 8.dp)
+    ) {
+        AsyncImage(
+            model = DataManager.userData.photoUrl,
+            contentDescription = "",
+            modifier = Modifier
+                .size(61.dp)
+                .clip(shape = CircleShape),
+        )
+
+        Column(
+            modifier = Modifier.padding(start = 13.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = DataManager.userData.username,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 13.sp
+            )
+            Text(
+                text = DataManager.userData.fullName,
+                color = Color.Gray,
+                fontSize = 13.sp
+            )
+            Text(
+                text = "Following",
+                color = Color.Gray,
+                fontSize = 12.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        IconButton(onClick = {}) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "",
+                tint = Color.Gray,
+                modifier = Modifier.size(15.dp)
+            )
+        }
+    }
+
 }
